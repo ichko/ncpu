12416ea8ef849fb9fafe2913c53e15efa8711955
Ubuntu <walas.piotr.prywanty@gmail.com>
2026-03-10 09:41:12 +0000

    scheduled ncpu

--- src/ncpu/runner.py
+++ src/ncpu/runner.py
@@ -1,51 +1,23 @@
 from typing import List, Optional
 
 from ncpu.dataset import NCPUDataset, PoolDataset, ScheduledDataset
-from ncpu.nca import NeuralCA
+from ncpu.nca import NeuralCAv2
 from ncpu.trainer import NCPUTrainer
 
-
-def setup_scheduled_trainer(configs: List = [], steps=2_000):
-    primary_config = configs[0]
-    datasets = [NCPUDataset(config) for config in configs]
-    dataset = ScheduledDataset(
-        datasets,
-        steps=steps
-        * primary_config.batch_size,  # this will make shift after N optim_step instead of N/batch_size optim_steps
-    )
-
-    nca = NeuralCA(
-        channels=primary_config.channels,
-        hidden_channels=primary_config.hidden_channels,
-        fire_rate=primary_config.fire_rate,
-        alive_masking=primary_config.alive_masking,
-        zero_initialization=primary_config.zero_initialization,
-    ).to(primary_config.device)
-
-    trainer = NCPUTrainer(
-        nca,
-        dataset.get_dataloader(batch_size=primary_config.batch_size),
-        lr=primary_config.lr,
-        gaussian_noise=primary_config.gaussian_noise,
-    )
-    trainer.sanity_check()
-
-    return trainer
-
-
-def setup_trainer(config):
+def setup_trainer(base_model, config):
     dataset = NCPUDataset(config)
     if config.pool_size > 0:
         dataset = PoolDataset(dataset, pool_size=config.pool_size)
 
-    nca = NeuralCA(
+    nca = NeuralCAv2(
         channels=config.channels,
         hidden_channels=config.hidden_channels,
         fire_rate=config.fire_rate,
-        alive_masking=config.alive_masking,
+        alive_threshold=config.alive_threshold,
         zero_initialization=config.zero_initialization,
     ).to(config.device)
 
+    nca = base_model
     trainer = NCPUTrainer(
         nca,
         dataset.get_dataloader(batch_size=config.batch_size),
--- src/ncpu/trainer.py
+++ src/ncpu/trainer.py
@@ -9,7 +9,7 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader
 
 from ncpu.base_trainer import BaseTrainer
-from ncpu.nca import NeuralCA
+from ncpu.nca import NeuralCAv2
 from ncpu.utils import (
     add_gaussian_noise,
     meshgrid_xy,
@@ -23,13 +23,14 @@ class NCPUTrainer(BaseTrainer):
 
     def __init__(
         self,
-        nca: NeuralCA,
+        nca: NeuralCAv2,
         dataloader: DataLoader,
         lr: float,
         gaussian_noise: float,
         stop_loss: Optional[float] = None,
     ):
         super().__init__()
+        
         self.nca = nca
         self.to(nca.device)
         self.dataloader = dataloader
@@ -39,6 +40,7 @@ class NCPUTrainer(BaseTrainer):
         self.stop_loss = stop_loss
         self.lr = lr
 
+        self.optim = torch.optim.Adam(self.nca.parameters(), lr=self.lr)
         left_mask, right_mask = self.ds.get_io_mask()
         self.inp_mask = torch.tensor(left_mask).to(self.nca.device)
         self.out_mask = torch.tensor(right_mask).to(self.nca.device)
@@ -78,7 +80,7 @@ class NCPUTrainer(BaseTrainer):
         first_state[:, -1] = yy
         return first_state
 
-    def optim_step(self, steps):
+    def optim_step_v2(self, steps):
         optim = torch.optim.Adam(self.nca.parameters(), lr=self.lr)
         batch = next(self.dataset_iter)
 
@@ -149,7 +151,69 @@ class NCPUTrainer(BaseTrainer):
 
         return info
 
-    def display_optim_step(self, info, display_size=None, to_show=8):
+    def optim_step(self, steps):
+        batch = next(self.dataset_iter)
+
+        inp, out = batch
+
+        inp = inp / 255.0
+        out = out / 255.0
+
+        if self.gaussian_noise > 0:
+            inp = add_gaussian_noise(inp, 0, self.gaussian_noise)
+
+        inp = inp.to(self.nca.device)
+        out = out.to(self.nca.device)
+
+        first_state = self._implant_input(inp)
+
+        forward_steps = steps
+        if isinstance(steps, (tuple, list)):
+            forward_steps = np.random.randint(steps[0], steps[1])
+
+        rollout = self.nca.forward(first_state, steps=forward_steps)
+        nca_out = rollout[:, -1, 0]
+
+        white_mask = (out > 0.5).float()
+        black_mask = 1 - white_mask
+
+        batch_loss = F.mse_loss(nca_out, out, reduction="none")
+        white_loss = batch_loss * white_mask
+        black_loss = batch_loss * black_mask
+
+        masks_sum = white_mask.sum() + black_mask.sum()
+        white_weight = black_mask.sum() / masks_sum
+        black_weight = white_mask.sum() / masks_sum
+
+        loss = (white_loss * white_weight + black_loss * black_weight).mean()
+
+        if torch.is_grad_enabled():
+            self.optim.zero_grad()
+            loss.backward()
+            self.optim.step()
+
+        metrics = {
+            "loss": loss.item(),
+            "white_loss": white_loss.sum().item(),
+            "black_loss": black_loss.sum().item(),
+        }
+        self.metrics.append(metrics)
+
+        if hasattr(self.dataloader, "update"):
+            self.dataloader.update((nca_out, out.detach()), loss)
+
+        info = {
+            "loss": loss.item(),
+            "metrics": metrics,
+            "inp": inp,
+            "out": out,
+            "nca_out": nca_out,
+            "rollout": rollout,
+        }
+
+        return info
+
+    def display_optim_step(self, info, display_size=64, to_show=8):
         fig, ax = plt.subplots(figsize=(8, 3))
         loss = [h["loss"] for h in self.metrics]
         ax.scatter(range(len(loss)), loss, s=1)
@@ -167,9 +231,8 @@ class NCPUTrainer(BaseTrainer):
         rollout = info["rollout"][:to_show]
         # read_only_channel = rollout[:to_show, -1, -1]
         io = torch.cat([inp, out, nca_out], dim=0)
-        if display_size is None:
-            display_size = inp.shape[1]
 
+        print(f"display size: {(display_size,display_size)}")
         media.show_images(
             io.detach().cpu(),
             columns=to_show,
@@ -183,6 +246,7 @@ class NCPUTrainer(BaseTrainer):
             rollout, columns=to_show, width=display_size, height=display_size, fps=10
         )
 
+    # piotr: can we remove this code? @iliya
     # def display_optim_step(self, info):
     #     fig, ax = plt.subplots(1, 1, figsize=(8, 4))
     #     ax.scatter(
