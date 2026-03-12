5c493f55445d3c1550fc2ed68171e24dc3a85f61
Iliya Zhechev <iliya.zhechev@gmail.com>
2026-03-12 21:39:24 +0200

    Legacy nca

--- src/ncpu/nca.py
+++ src/ncpu/nca.py
@@ -81,3 +81,81 @@ class NeuralCA(nn.Module):
         seq = seq.permute(1, 0, 2, 3, 4)
 
         return seq  # (batch, time, channels, height, width)
+
+
+class NeuralCALegacy(nn.Module):
+    def alive(self, x):
+        # return F.max_pool2d(x[:, :1, :, :], kernel_size=3, stride=1, padding=0) > 0.1
+        return (
+            F.max_pool2d(abs(0.5 - x[:, :1, :, :]), kernel_size=3, stride=1, padding=0)
+            > 0.1
+        )
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def __init__(
+        self,
+        channels,
+        hidden_channels,
+        fire_rate,
+        alive_masking_flag,
+        zero_initialization,
+    ) -> None:
+        super().__init__()
+        sobel_x = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) / 8
+        sobel_y = torch.tensor([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]) / 8
+        identity = torch.tensor([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+
+        all_filters = torch.stack((identity, sobel_x, sobel_y))
+        all_filters_batch = all_filters.repeat(channels, 1, 1).unsqueeze(1)
+        all_filters_batch = nn.Parameter(all_filters_batch, requires_grad=False)
+
+        self.channels = channels
+        self.all_filters_batch = all_filters_batch
+        self.rule = nn.Sequential(
+            nn.Conv2d(3 * channels, hidden_channels, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_channels, channels, kernel_size=1, bias=False),
+        )
+        self.fire_rate = fire_rate
+        self.alive_masking_flag = alive_masking_flag
+
+        if zero_initialization:
+            nn.init.zeros_(self.rule[-1].weight)
+
+    def perception(self, x, pad_type):
+        delta = F.conv2d(
+            F.pad(x, (1, 1, 1, 1), pad_type),
+            self.all_filters_batch,
+            groups=self.channels,
+        )
+        return delta
+
+    def alive_masking(self, x, x_padded, pad_type):
+        if self.alive_masking_flag:
+            pre_life_mask = self.alive(x_padded)
+            post_life_mask = self.alive(F.pad(x, (1, 1, 1, 1), pad_type))
+            life_mask = (pre_life_mask & post_life_mask).to(x.dtype)
+            x = x * life_mask
+        return x
+
+    def forward(self, x, steps=1):
+        seq = [x]
+
+        # pad_type = "circular"
+        pad_type = "constant"
+
+        for _ in range(steps):
+            x_padded = F.pad(x, (1, 1, 1, 1), pad_type)
+            delta = self.perception(x, pad_type)
+            delta = self.rule(delta)
+            x = x + delta
+            x = self.alive_masking(x, x_padded, pad_type)
+
+            seq.append(x)
+
+        seq = torch.stack(seq)
+        seq = seq.permute(1, 0, 2, 3, 4)
+        return seq
